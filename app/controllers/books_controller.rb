a9b128e21e063380a3236da916aec44e973b0de3
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-03-13 10:08:21 +0000

    3-12 投稿フォーム作成中1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = List.new  
   end
+ def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+ end
+
 
   def index
+    
   end
 
   def show
@@ -11,3 +23,9 @@ class BooksController < ApplicationController
   def edit
   end
 end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
