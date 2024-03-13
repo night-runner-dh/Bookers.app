d427d3b59d6d5b517809013e734a3a42bcf799ab
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-03-13 16:15:27 +0000

    3-12 ｽﾀｲﾙｼｰﾄまだ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,57 @@
 class BooksController < ApplicationController
   def new
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @book = List.new  
+    @list = List.new
   end
+  
  def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    @book = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
+    if @list.save
+      flash[:notice] = "投稿に成功しました。"
+
+    
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+       redirect_to book_path(@list.id)
+    else
+    # 3. フラッシュメッセージを定義し、new.html.erbを描画する
+       flash[:alert] = "投稿に失敗しました。" #キーをalertに変更
+       #render :index
+       redirect_to "/books"
+    end
  end
 
 
   def index
-    
+     @list = List.new
+     @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-end
-
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to book_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new'
   get '/top' => 'homes#top'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  post 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
+  
    resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
