4d0f1c84fec385f28f9fa3aadc09fdb0ac95b1ce
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-03-14 05:49:17 +0000

    3-12 テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,62 +1,59 @@
 class BooksController < ApplicationController
   def new
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.new
+    @book = Book.new
   end
   
  def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    @list = List.new(list_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    if @list.save
+    if @book.save
       flash[:notice] = "Book was successfully created."
 
-    
-    # 4. トップ画面へリダイレクト
-       redirect_to book_path(@list.id)
+           redirect_to book_path(@book.id)
     else
-    # 3. フラッシュメッセージを定義し、new.html.erbを描画する
-       flash[:alert] = "投稿に失敗しました。" #キーをalertに変更
-       #render :index
-       redirect_to "/books"
+    
+       #render "/books/index"はエラーでダメ
+      @books = Book.all
+       render :index
     end
  end
 
 
   def index
-     @list = List.new
-     @lists = List.all
+     @book = Book.new
+     @books = Book.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @list = List.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
   def update
-  @list = List.find(params[:id])
-    if @list.update(list_params)
+  @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(@list.id)
+    redirect_to book_path(@book.id)
     else
-    flash.now[:alert] = "編集に失敗しました。"
-    render 'edit'
+       render :edit
     end
   end
 
   def destroy
-    list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to '/books',notice: '削除されました'  # 投稿一覧画面へリダイレクト  
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books',notice: 'Book was successfully destroyed.'  # 投稿一覧画面へリダイレクト  
   end
   
   private
   # ストロングパラメータ
-  def list_params
-    params.require(:list).permit(:title, :body)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get '/top' => 'homes#top'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  post 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
   
   root :to => 'homes#top'
   
